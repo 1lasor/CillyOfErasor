09e61087908a8067a6e5249bf786a700ef0c103c
1lasor <2244602447@qq.com>
2025-04-04 18:07:11 +0800

    basic code

---
+++ eval.py
@@ -0,0 +1,397 @@
+from lexical_analyzer import cilly_lexer,error
+from syntactic_analyzer import cilly_parser
+
+def mk_num(i):
+    return ['num', i]
+
+
+def mk_str(s):
+    return ['str', s]
+
+
+TRUE = ['bool', True]
+FALSE = ['bool', False]
+
+
+def mk_bool(b):
+    return TRUE if b else FALSE
+
+
+def mk_proc(params, body):
+    return ['proc', params, body]
+
+
+NULL = ['null', None]
+
+
+def val(v):
+    return v[1]
+
+
+def lookup_var(env, var):
+    if var not in env:
+        error('lookup var', f'未定义变量{var}')
+
+    return env[var]
+
+
+def set_var(env, var, val):
+    if var not in env:
+        error('set var', f'未定义变量{var}')
+
+    env[var] = val
+
+
+def define_var(env, var, val):
+    if var in env:
+        error('define var', f'变量已定义{var}')
+
+    env[var] = val
+
+
+env = {}
+
+
+def cilly_eval(ast):
+    def err(msg):
+        return error('cilly eval', msg)
+
+    def ev_program(node):
+        _, statements = node
+
+        r = NULL
+
+        for s in statements:
+            r = visit(s)
+
+        return r
+
+    def ev_expr_stat(node):
+        _, e = node
+        return visit(e)
+
+    def ev_print(node):
+        _, args = node
+
+        for a in args:
+            print(val(visit(a)), end=' ')
+
+        print('')
+
+        return NULL
+
+    def ev_literal(node):
+        tag, v, _, _ = node
+
+        if tag in ['num', 'str']:
+            return node
+
+        if tag in ['true', 'false']:
+            return TRUE if tag == 'true' else FALSE
+
+        if tag == 'null':
+            return NULL
+
+        err(f'非法字面量{node}')
+
+    def ev_unary(node):
+        _, op, e = node
+
+        v = val(visit(e))
+
+        if op == '-':
+            return mk_num(-v)
+
+        if op == '!':
+            return FALSE if v else TRUE
+
+        err(f'非法一元运算符{op}')
+
+    def ev_binary(node):
+        _, op, e1, e2 = node
+
+        v1 = val(visit(e1))
+        if op == '&&':
+            if v1 == False:
+                return FALSE
+            else:
+                return visit(e2)
+
+        if op == '||':
+            if v1 == True:
+                return TRUE
+            else:
+                return visit(e2)
+
+        v2 = val(visit(e2))
+
+        if op == '+':
+            return mk_num(v1 + v2)
+
+        if op == '-':
+            return mk_num(v1 - v2)
+
+        if op == '*':
+            return mk_num(v1 * v2)
+
+        if op == '/':
+            return mk_num(v1 / v2)
+            
+        if op == '^':
+            return mk_num(v1 ** v2)
+
+        if op == '>':
+            return mk_bool(v1 > v2)
+        if op == '>=':
+            return mk_bool(v1 >= v2)
+        if op == '<':
+            return mk_bool(v1 < v2)
+        if op == '<=':
+            return mk_bool(v1 <= v2)
+        if op == '==':
+            return mk_bool(v1 == v2)
+        if op == '!=':
+            return mk_bool(v1 != v2)
+
+        err(f'非法二元运算符{op}')
+        
+    def ev_ternary(node):
+        _, cond, true_expr, false_expr = node
+        
+        if visit(cond) == TRUE:
+            return visit(true_expr)
+        else:
+            return visit(false_expr)
+
+    def ev_if(node):
+        _, cond, true_s, false_s = node
+
+        if visit(cond) == TRUE:
+            return visit(true_s)
+
+        if false_s != None:
+            return visit(false_s)
+
+        return NULL
+
+    def ev_while(node):
+        _, cond, body = node
+
+        r = NULL
+        prev_r = NULL
+        while visit(cond) == TRUE:
+            r = visit(body)
+            if r[0] == 'break':
+                r = prev_r
+                break
+
+            if r[0] == 'continue':
+                continue
+            prev_r = r
+
+        return r
+        
+    def ev_for(node):
+        _, init, cond, incr, body = node
+        
+        r = NULL
+        prev_r = NULL
+        
+        visit(init)  # 执行初始化语句
+        
+        while True:
+            cond_val = visit(cond)
+            if cond_val[0] == 'expr_stat':
+                cond_val = cond_val[1]
+                
+            if cond_val != TRUE:
+                break
+                
+            r = visit(body)
+            if r[0] == 'break':
+                r = prev_r
+                break
+                
+            if r[0] == 'continue':
+                visit(incr)
+                continue
+                
+            prev_r = r
+            visit(incr)  # 执行增量语句
+            
+        return r
+
+    def ev_break(node):
+        return ['break']
+
+    def ev_continue(node):
+        return ['continue']
+
+    def ev_block(node):
+        _, statements = node
+
+        r = NULL
+
+        for s in statements:
+            r = visit(s)
+            if r[0] in ['break', 'continue']:
+                return r
+
+        return r
+
+    def ev_id(node):
+        _, name, _, _ = node
+
+        return lookup_var(env, name)
+
+    def ev_define(node):
+        _, name, e = node
+        v = visit(e)
+
+        define_var(env, name, v)
+        return NULL
+
+    def ev_assign(node):
+        _, name, e = node
+        v = visit(e)
+
+        set_var(env, name, v)
+        return NULL
+
+    def ev_return(node):
+        _, e = node
+
+        if e != None:
+            return visit(e)
+        else:
+            return NULL
+
+    def ev_fun(node):
+        _, params, body = node
+        return mk_proc(params, body)
+
+    def ev_fun_def(node):
+        _, name, params, body = node
+        proc = mk_proc(params, body)
+        define_var(env, name, proc)
+        return NULL
+
+    def ev_call(node):
+        _, f_expr, args = node
+
+        p = visit(f_expr)
+        if p[0] != 'proc':
+            err(f'非法函数{p}')
+
+        _, params, body = p
+
+        args = [visit(a) for a in args]
+
+        global env
+        old = env
+
+        env = {p: a for (p, a) in zip(params, args)}
+
+        v = visit(body)
+
+        env = old
+
+        return v
+
+    visitors = {
+        'program': ev_program,
+        'expr_stat': ev_expr_stat,
+        'print': ev_print,
+        'if': ev_if,
+        'while': ev_while,
+        'for': ev_for,
+        'break': ev_break,
+        'continue': ev_continue,
+        'block': ev_block,
+
+        'define': ev_define,
+        'assign': ev_assign,
+
+        'unary': ev_unary,
+        'binary': ev_binary,
+        'ternary': ev_ternary,
+
+        'return': ev_return,
+        'fun': ev_fun,
+        'fun_def': ev_fun_def,
+        'call': ev_call,
+
+        'id': ev_id,
+        'num': ev_literal,
+        'str': ev_literal,
+        'true': ev_literal,
+        'false': ev_literal,
+        'null': ev_literal,
+    }
+
+    def visit(node):
+        tag = node[0]
+        if tag not in visitors:
+            err(f'非法节点{node}')
+
+        return visitors[tag](node)
+
+    return visit(ast)
+
+
+if __name__ == "__main__":
+    test_cases = [
+        # 测试基本语法和运算
+        '''
+        var sum = 0;
+        var i = 1;
+        
+        while(i <= 100){
+            sum = sum + i;
+            i = i + 1;
+        }
+        
+        print("1+2+...100=",sum);
+        ''',
+        
+        # 测试幂运算
+        '''
+        var x = 2 ^ 3 ^ 2;
+        print("2^3^2=", x);
+        ''',
+        
+        # 测试三元表达式
+        '''
+        var x = -5;
+        var y = x > 0 ? x : -x;
+        print("绝对值:", y);
+        ''',
+        
+        # 测试for循环
+        '''
+        for (var i = 0; i < 10; i = i + 1;) {
+            print("循环计数:", i);
+        }
+        ''',
+        
+        # 测试函数
+        '''
+        var add = fun(a,b){
+            return a + b;
+        };
+        
+        print("函数测试:", add(1,2), add(3*4, 6));
+        '''
+    ]
+    
+    for i, test in enumerate(test_cases):
+        print(f"\n测试 {i+1}:")
+        tokens = cilly_lexer(test)
+        print("tokens:", tokens)
+        
+        ast = cilly_parser(tokens)
+        print("ast:", ast)
+        
+        print("执行结果:")
+        env = {}  # 重置环境
+        v = cilly_eval(ast)
---
+++ syntactic_analyzer.py
@@ -0,0 +1,468 @@
+'''
+cilly 语法
+
+program : statement* EOF
+
+statement
+    : define_statement
+    | assign_statement
+    | print_statement
+    | if_statement
+    | while_statement
+    | for_statement
+    | continue_statement
+    | break_statement
+    | return_statement
+    | fun_statement
+    | block_statement
+    | expr_statement
+    ;
+    
+define_statement
+    : 'var' ID '=' expr ';'
+    ;
+    
+assign_statement
+    : ID '=' expr ';'
+    ;
+    
+print_statement
+    : 'print' '(' args? ')' ';'
+    ;
+    
+args : expr (',' expr)* ;
+
+if_statement
+    : 'if' '(' expr ')' statement ('else' statement)?
+    ;
+    
+while_statement
+    : 'while' '(' expr ')' statement
+    ;
+    
+for_statement
+    : 'for' '(' statement statement statement ')' statement
+    ;
+    
+continue_statement
+    : 'continue' ';'
+    ;
+    
+break_statement
+    : 'break' ';'
+    ;
+    
+return_statement
+    : 'return' expr? ';'
+    ;
+    
+fun_statement
+    : 'fun' ID '(' params? ')' block_statement
+    ;
+    
+block_statement
+    : '{' statement* '}'
+    ;
+    
+expr_statement:
+    : expr ';'
+    
+expr
+    : id | num | string | 'true' | 'false' | 'null'
+    | '(' expr ')'
+    | ('-' | '!') expr
+    | expr ('+' | '-' | '*' | '/' | '==' | '!=' | '>' | '>=' | '<' | '<=' | '&&' | '||') expr
+    | expr '?' expr ':' expr
+    | expr '^' expr
+    | 'fun' '(' params? ')' block_statement
+    | expr '(' args? ')'
+    ;
+'''
+
+from lexical_analyzer import error, mk_tk, tk_tag, tk_val, cilly_lexer
+
+EOF = mk_tk('eof')
+
+def make_token_reader(ts, err):
+    pos = -1
+    cur = None
+    
+    def peek(p=0):
+        if pos + p >= len(ts):
+            return 'eof'
+        else:
+            return tk_tag(ts[pos + p])
+        
+    def match(t):
+        if peek() != t:
+            err(f'期望{t},实际为{cur}', cur)
+            
+        return next()
+    
+    def next():
+        nonlocal pos, cur
+        
+        old = cur
+        pos = pos + 1
+        
+        if pos >= len(ts):
+            cur = EOF
+        else:
+            cur = ts[pos]
+            
+        return old
+    next()
+    
+    return peek, match, next
+        
+def cilly_parser(tokens):
+    def err(msg, token=None):
+        error('cilly parser', msg, token)
+        
+    peek, match, next = make_token_reader(tokens, err)
+    
+    def program():
+        
+        r = []
+        
+        while peek() != 'eof':
+            r.append( statement() )
+            
+        return ['program', r]
+    
+    def statement():
+        t = peek()
+        
+        if t == 'var':
+            return define_stat()
+        
+        if t == 'id' and peek(1) == '=':
+            return assign_stat()
+        
+        if t == 'print':
+            return print_stat()
+        
+        if t == 'if':
+            return if_stat()
+        
+        if t == 'while':
+            return while_stat()
+        
+        if t == 'for':
+            return for_stat()
+        
+        if t == 'break':
+            return break_stat()
+        
+        if t == 'continue':
+            return continue_stat()
+        
+        if t == 'return':
+            return return_stat()
+        
+        if t == 'fun' and peek(1) == 'id':
+            return fun_stat()
+        
+        if t == '{':
+            return block_stat()
+        
+        return expr_stat()
+    
+    def define_stat():
+        match('var')
+        
+        id = tk_val ( match('id') )
+        
+        match('=')
+        
+        e = expr()
+        
+        match(';')
+        
+        return ['define', id, e]
+    
+    def assign_stat():
+        id = tk_val( match('id') )
+        
+        match('=')
+        
+        e = expr()
+        
+        match(';')
+        
+        return ['assign', id, e]
+    
+    def print_stat():
+        match('print')
+        match('(')
+        
+        if peek() == ')':
+            alist = []
+        else:
+            alist = args()
+            
+        match(')')
+        match(';')
+        
+        return ['print', alist]
+    
+    def args():
+        
+        r = [expr()]
+        
+        while peek() == ',':
+            match(',')
+            r.append( expr() )
+            
+        return r
+    
+    def if_stat(): # if ( expr ) statement (else statment)?
+        match('if')
+        match('(')
+        cond = expr()
+        match(')')
+        
+        true_stat = statement()
+        
+        if peek() == 'else':
+            match('else')
+            false_stat = statement()
+        else:
+            false_stat = None
+        return ['if', cond , true_stat, false_stat]
+    
+    def while_stat():
+        match('while')
+        match('(')
+        cond = expr()
+        match(')')
+        body = statement()
+        
+        return ['while', cond, body]
+    
+    # for (var i = 0; i < 10; i = i + 1；) {
+    #         print(i);
+    #     }
+
+    # for_statement
+    # : 'for' '(' statement statement statement ')' statement
+    # ;
+    def for_stat():
+        match('for')                     # 匹配for关键字
+        match('(')                       # 匹配左括号
+        
+        init = statement()               # 解析初始化语句，如"var i = 0;"
+        cond = statement()                      # 解析条件表达式，如"i < 10"                    
+        incr = statement()                      # 解析增量表达式，如"i = i + 1"
+        
+        match(')')                       # 匹配右括号
+        body = statement()               # 解析循环体语句，可以是单个语句或语句块
+        
+        return ['for', init, cond, incr, body]  # 返回AST节点，表示for循环结构
+    
+    def continue_stat():
+        match('continue')
+        match(';')
+        
+        return ['continue']
+    
+    def break_stat():
+        match('break')
+        match(';')
+        return ['break']
+    
+    def return_stat():
+        match('return')
+        
+        if peek() != ';':
+            e = expr()
+        else:
+            e = None
+            
+        match(';')
+        
+        return ['return', e]
+    
+    def fun_stat():
+        match('fun')
+        name = tk_val(match('id'))
+        
+        match('(')
+        if peek() == ')':
+            plist = []
+        else:
+            plist = params()
+        match(')')
+        
+        body = block_stat()
+        
+        return ['fun_def', name, plist, body]
+    
+    def block_stat():
+        match('{')
+        
+        r = []
+        
+        while peek() != '}':
+            r.append( statement() )
+            
+        match('}')
+        
+        return ['block', r]
+    
+    def expr_stat():
+        e = expr()
+        match(';')
+        
+        return ['expr_stat', e]
+    
+    def literal(bp=0):
+        return next()
+    
+    def unary(bp):
+        op = tk_tag( next() )
+        e = expr(bp)
+        
+        return ['unary', op, e]
+    
+    def fun_expr(bp=0):
+        match('fun')
+        match( '(' )
+        if peek() == ')':
+            plist = []
+        else:
+            plist = params()
+            
+        match(')')
+        body = block_stat()
+        
+        return ['fun', plist, body]
+    
+    def params():
+        r = [ tk_val( match('id') )]
+        
+        while peek() == ',':
+            match(',')
+            r.append ( tk_val( match('id') ) )
+            
+        return r
+    
+    def parens(bp=0):
+        match('(')
+        
+        e = expr()
+        
+        match(')')
+        
+        return e
+    
+    def ternary(left, bp):
+        match('?')
+        true_expr = expr(0)
+        match(':')
+        false_expr = expr(bp)
+        
+        return ['ternary', left, true_expr, false_expr]
+    
+    def power(left, bp):
+        match('^')
+        right = expr(bp)
+        
+        return ['binary', '^', left, right]
+    
+    op1 = {
+        'id': (100, literal),
+        'num': (100, literal),
+        'str': (100, literal),
+        'true': (100, literal),
+        'false': (100, literal),
+        'null': (100, literal),
+        '-': (85, unary),
+        '!': (85, unary),
+        'fun': (98, fun_expr),
+        '(': (100, parens),
+    }
+    
+    def get_op1_parser(t):
+        if t not in op1:
+            err(f'非法token: {t}')
+            
+        return op1[t]
+        
+    def binary(left, bp):
+        op = tk_tag( next() )
+        right = expr(bp)
+        
+        return ['binary', op, left, right]
+    
+    def call(fun_expr, bp=0):
+        match('(')
+        if peek() != ')':
+            alist = args()
+        else:
+            alist = []
+        match(')')
+        return ['call', fun_expr, alist]
+    
+    op2 = {
+        '^': (88, 87, power),
+        '*': (80, 81, binary),
+        '/': (80, 81, binary),
+        '+': (70, 71, binary),
+        '-': (70, 71, binary),
+        '>': (60, 61, binary),
+        '>=': (60, 61, binary),
+        '<': (60, 61, binary),
+        '<=': (60, 61, binary),
+        '==': (50, 51, binary),
+        '!=': (50, 51, binary),
+        '&&': (40, 41, binary),
+        '||': (30, 31, binary),
+        '?': (20, 21, ternary),
+        '(': (90, 91, call),
+    }
+    
+    def get_op2_parser(t):
+        if t not in op2:
+            return (0,0,None)
+        else:
+            return op2[t]
+            
+    def expr(bp = 0):
+        r_bp, parser = get_op1_parser( peek() )
+        left = parser(r_bp)
+        
+        while True:
+            l_bp, r_bp, parser = get_op2_parser( peek() )
+            if parser == None or l_bp <= bp:
+                break
+            
+            left = parser(left, r_bp)
+        
+        return left
+    
+    return program()
+
+if __name__ == "__main__":
+    from lexical_analyzer import cilly_lexer
+    
+    test_cases = [
+        "2 ^ 3 ^ 2;#幂表达式",
+        "x > 0 ? x : -x;#？表达式",
+        """
+        # for 循环语句
+        for (var i = 0; i < 10; i = i + 1;) {
+            print(i);
+        }
+        """,
+        "#下面为报错时行列号提示：",
+        "2 ^ 3 ^ 2;幂表达式"
+    ]
+    
+    for i, test in enumerate(test_cases):
+        print(f"\n测试 {i+1}:")
+        tokens = cilly_lexer(test)
+        print("tokens:", tokens)
+        
+        ast = cilly_parser(tokens)
+        print("ast:", ast)
